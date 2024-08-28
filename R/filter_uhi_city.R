3bf2d658ba1e8b3402f2e82dd98aa7301ea38d23
Andy <andypicke@gmail.com>
2024-08-28 14:23:58 -0600

    fix function

--- R/filter_uhi_city.R
+++ R/filter_uhi_city.R
@@ -4,11 +4,8 @@ filter_uhi_city <- function(uhi, wh_city){
   
   uhi_city <- uhi |> filter(city == wh_city)
   
-  if (nchar(uhi_city$census_tract_number[1]) == 10) {
-    uhi_city <- uhi_city |> mutate(GEOID = paste0("0", census_tract_number)) 
-  } else {
-    uhi_city <- uhi_city |> mutate(GEOID = census_tract_number) 
-  }
+  uhi_city <- uhi_city |> mutate(GEOID = if_else(nchar(census_tract_number) == 10, paste0("0", census_tract_number), census_tract_number))
+  
   # get state FIPS code
   uhi_city <- uhi_city |> mutate(state_fips = substr(GEOID,1,2))
   # get county FIPS code
