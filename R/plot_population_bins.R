7ba33cc06aec1a8788c49a19c807432b71c87632
Andy <andypicke@gmail.com>
2024-08-30 08:16:50 -0600

    add function to make population chart

---
+++ R/plot_population_bins.R
@@ -0,0 +1,22 @@
+
+
+plot_population_bins <- function(dat_joined){
+  
+  # bin the UHI effect values in 1 deg bins
+  dat_joined <- dat_joined |> mutate(bin = ggplot2::cut_width(uhi_effect_degF, width = 1,center = 0.5))
+
+    g <- x |> 
+    group_by(dat_joined) |>
+    summarise(tot_pop = sum(value)) |>
+    ggplot(aes(bin, tot_pop)) +
+    geom_col(fill = "orange", color = "black") +
+    scale_y_continuous(labels = scales::comma) +
+    labs(x = "Urban Heat Island Effect [deg F]",
+         y = "Population",
+         caption = "Data from Climate Central",
+         title = "Urban Heat Island Effect by Population") +
+    theme_minimal(base_size = 14)
+  
+  return(g)
+  
+}
